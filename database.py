f120b1628839a9d28c8e50c0e94539dc86624972
the-sam-sepiol <152436775+the-sam-sepiol@users.noreply.github.com>
2025-06-18 10:15:53 -0400

    Aligned database.py schema with scheme found in Status Report 1, Created "Customer" Role, Need to discuss access for other roles.

--- database.py
+++ database.py
@@ -35,34 +35,46 @@ def get_db_connection():
         password=DB_PASS,
         port=DB_PORT
     )
-    print("Successfully connected to the database.")
+    print('Successfully connected to the database.')
     return conn
 
-def create_tables():
+def create_tables_roles():
 
     conn = None
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    cursor.execute("CREATE TABLE Category (category_id INT PRIMARY KEY, category_name VARCHAR(100))");
-    cursor.execute("CREATE TABLE Author (author_id INT PRIMARY KEY, author_name VARCHAR(100))");
-    cursor.execute("CREATE TABLE Student (student_id INT PRIMARY KEY, student_name VARCHAR(100))");
+    cursor.execute('CREATE TABLE IF NOT EXISTS Category (category_id INT PRIMARY KEY, category_name VARCHAR(100))')
+    cursor.execute('CREATE TABLE IF NOT EXISTS Author (author_id INT PRIMARY KEY, author_name VARCHAR(100))')
 
-    cursor.execute("CREATE TABLE Book (" +
-                "book_id INT PRIMARY KEY, " +
-                "title VARCHAR(100), " +
-                "author_id INT, " +
-                "category_id INT, " +
-                "FOREIGN KEY(author_id) REFERENCES Author(author_id), " +
-                "FOREIGN KEY(category_id) REFERENCES Category(category_id))");
+    cursor.execute('CREATE TABLE IF NOT EXISTS Book (' +
+                'book_id INT PRIMARY KEY, ' +
+                'title VARCHAR(100), ' +
+                'author_id INT, ' +
+                'category_id INT, ' +
+                'price INT, ' +
+                'FOREIGN KEY(author_id) REFERENCES Author(author_id), ' +
+                'FOREIGN KEY(category_id) REFERENCES Category(category_id))')
 
-    cursor.execute("CREATE TABLE Inventory (book_id INT PRIMARY KEY, Quantity INTEGER, FOREIGN KEY(book_id) REFERENCES Book(book_id))");
-    cursor.execute("CREATE TABLE student_book_tr (" +
-                "transaction_id INT PRIMARY KEY, " +
-                "student_id INT, " +
-                "book_id INT, " +
-                "FOREIGN KEY(student_id) REFERENCES Student(student_id), " +
-                "FOREIGN KEY(book_id) REFERENCES Book(book_id))");
+    cursor.execute('CREATE TABLE IF NOT EXISTS Customers (' +
+                   'customer_id INT PRIMARY KEY, ' +
+                   'customer_name VARCHAR(100)')
+
+    cursor.execute('CREATE TABLE IF NOT EXISTS Inventory (book_id INT PRIMARY KEY, Quantity INTEGER, FOREIGN KEY(book_id) REFERENCES Book(book_id))')
+    cursor.execute('CREATE TABLE IF NOT EXISTS Transactions (' +
+                'transaction_id INT PRIMARY KEY, ' +
+                'transaction_total INT, ' + 
+                'transaction_date DATE, ' + 
+                'FOREIGN KEY(customer_id) REFERENCES Customers(customer_id), ' +
+                'FOREIGN KEY(book_id) REFERENCES Book(book_id))')
+    
+
+    #create role customer
+    cursor.execute('CREATE ROLE Customer')
+    cursor.execute('GRANT SELECT ON Category, Author, Book, Inventory TO Customer')
+    cursor.execute('GRANT SELECT (transaction_total, transaction_date) ON Transactions TO Customer')
+
+
     
     conn.commit()
     cursor.close()
@@ -70,6 +82,6 @@ def create_tables():
 
 if __name__ == '__main__':
     initialize_db()
-    create_tables()
+    create_tables_roles()
 
 

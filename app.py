003f4f8c361bfd16237aafcfae984c0d30175a33
the-sam-sepiol <152436775+the-sam-sepiol@users.noreply.github.com>
2025-07-08 17:37:26 -0400

    3 things:
    Made a home page (I took index.html and added some stuff) namely: books under $20 and books under $10
    
    Added image_id to the books table, it is going to be the filename for the books
    
    Added a drop_tables() funct to database.py, this is for if we make changes to the DB, you must drop all tables if we want to edit columns

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
 from flask import Flask, render_template, request
+from database import get_db_connection
+import psycopg2.extras
+
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
+def decision():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    con = get_db_connection()
+    cursor = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    cursor.execute("""        
+        SELECT b.book_id, b.title, b.price, b.image_id, a.author_name
+        FROM Book b
+        JOIN Author a ON b.author_id = a.author_id
+        WHERE b.price > 10 AND b.price < 20
+        ORDER BY b.price ASC
+    """)
+
+    books_under_20 = cursor.fetchall()
+
+    cursor.execute("""
+        SELECT b.book_id, b.title, b.price, b.image_id, a.author_name
+        FROM Book b
+        JOIN Author a ON b.author_id = a.author_id
+        WHERE b.price < 10
+        ORDER BY b.price ASC           
+    """)
+
+    books_under_10 = cursor.fetchall()
+
+    cursor.close()
+    con.close()
+
+    return render_template('home.html', books_under_10 = books_under_10, books_under_20 = books_under_20)
 
 
 
